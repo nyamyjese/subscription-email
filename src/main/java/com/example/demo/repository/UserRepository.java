8c3f52cdb1a65e2e0e336786050141556bb7b009
nyamyjese <hei.jese.2@gmail.com>
2026-07-10 12:05:08 +0100

    feat(repository) : add user repository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.UserEntity;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {}
